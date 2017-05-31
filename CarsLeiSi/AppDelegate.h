1a721812c9a9490c68487651008ef35fa4f42f95
MacBookXcZl <xczl@macbookxczldemacbook-pro.local>
2017-05-31 11:43:43 +0800

    Initial Commit

---
+++ CarsLeiSi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarsLeiSi
+//
+//  Created by MacBookXcZl on 2017/5/31.
+//  Copyright © 2017年 MacBookXcZl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
